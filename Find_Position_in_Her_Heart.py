6655e8b4547480dd9d6a1ab09eeead3b3635e23b
ThyStranger <128230055+ThyStranger@users.noreply.github.com>
2024-10-09 15:34:31 +0800

    Update Find_Position_in_Her_Heart.py

--- Find_Position_in_Her_Heart.py
+++ Find_Position_in_Her_Heart.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from scipy.spatial.transform import Rotation as R
 v=[np.array([0,0,0])] #Velocity in x,y,z
 s=[[0,0,0]] #Displacement in x,y,z
+sx=[0]
+sy=[0]
+sz=[0]
 a=[[0,0,0]]
 w=[np.array([0,0,0])] #Angular velocity in x,y,z, from gyroscope
 sy_lab=[]
@@ -38,8 +42,10 @@ def motion(ax,ay,az,t):
     #Integrating acceleration and velocity along original axises, using trapezoidal rule
     v.append(0.5*(a[-1]+a[-2])*(t-ta[-1])+v[-1])
     s.append(0.5*(v[-1]+v[-2])*(t-ta[-1])+s[-1])
+    sx.append(s[-1][0])
+    sy.append(s[-1][1])
+    sz.append(s[-1][2])
     
-
 def calculate_lab_motion(g):
     down_direction=g/np.linalg.norm(g)
     for i in s:
@@ -56,30 +62,66 @@ for i in range(1000):
     tg.append(i/100)
 
 calculate_lab_motion(g)
-plt.subplot(2,2,1)
-plt.plot(ta,s)
-plt.xlabel('t/s',fontsize=18)
-plt.ylabel('Displacement in accelerometer axises at t=0/ m',fontsize=10)
+# plt.subplot(2,2,1)
+# plt.plot(ta,s)
+# plt.xlabel('t/s',fontsize=18)
+# plt.ylabel('Displacement in accelerometer axises at t=0/ m',fontsize=10)
 
-plt.subplot(2,2,2)
-plt.plot(ta,sy_lab)
-plt.xlabel('t/s',fontsize=18)
-plt.ylabel('Vertical Displacement/ m',fontsize=15)
+# plt.subplot(2,2,2)
+# plt.plot(ta,sy_lab)
+# plt.xlabel('t/s',fontsize=18)
+# plt.ylabel('Vertical Displacement/ m',fontsize=15)
 
-plt.subplot(2,2,3)
-plt.plot(ta,sxz_lab)
-plt.xlabel('t/s',fontsize=18)
-plt.ylabel('Horizontal Displacement/ m',fontsize=15)
+# plt.subplot(2,2,3)
+# plt.plot(ta,sxz_lab)
+# plt.xlabel('t/s',fontsize=18)
+# plt.ylabel('Horizontal Displacement/ m',fontsize=15)
 
-plt.subplot(2,2,4)
-rotvec=[]
-for i in theta:
-    if np.linalg.norm(i)==0:
-        rotvec.append([0,0,0])
-        continue
-    rotvec.append(i/np.linalg.norm(i))
-plt.plot(tg,rotvec)
-plt.xlabel('t/s',fontsize=18)
-plt.ylabel('Rotation Vector Elements',fontsize=15)
+# plt.subplot(2,2,4)
+# rotvec=[]
+# for i in theta:
+#     if np.linalg.norm(i)==0:
+#         rotvec.append([0,0,0])
+#         continue
+#     rotvec.append(i/np.linalg.norm(i))
+# plt.plot(tg,rotvec)
+# plt.xlabel('t/s',fontsize=18)
+# plt.ylabel('Rotation Vector Elements',fontsize=15)
+# plt.show()
 
+# ax=plt.axes(projection='3d')
+# ax.plot3D(sx,sy,sz)
+# print(s[-1][0]-sx[-1])
+# plt.show()
+
+# fig=plt.figure()
+# ax = fig.add_subplot(projection='3d')
+# ax.set_xlim3d((0, np.max(sx) + np.ptp(sx)*0.05))
+# ax.set_ylim3d((np.min(sy) - np.ptp(sy)*0.05, np.max(sy) + np.ptp(sy)*0.05))
+# ax.set_zlim3d((np.min(sz) - np.ptp(sz)*0.05, np.max(sz) + np.ptp(sz)*0.05))
+# line, = ax.plot([], [], lw=2)
+# point, = ax.plot([], [], 'ro')
+
+# def init():
+#   line.set_data([], [])
+#   point.set_data([], [])
+#   return line, point
+
+# def update(frame):
+#   line.set_data(sx[:frame], sy[:frame], sz[:frame])
+#   point.set_data(sx[frame], sy[frame],sz[:frame])
+#   return line, point
+# N=100
+# # data = np.array(list(gen(N))).T
+# ani = FuncAnimation(fig, update, N, frames=len(sx), interval=10000/N, blit=False)
+# plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+line, = ax.plot(sx, sy, sz)
+def update(num, x, y, z, line):
+    line.set_data(x[:num], y[:num])
+    line.set_3d_properties(z[:num])
+    return line,
+ani = FuncAnimation(fig, update, frames=len(t), fargs=[x, y, z, line], interval=100)
 plt.show()
